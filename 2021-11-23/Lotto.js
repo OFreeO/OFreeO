f44487d682be819aaece92c137193c7cb6a64bae
FreeO <kwon6772@naver.com>
2021-11-23 16:40:57 +0900

    2021-11-23공부함
    글자 움직이는거 꿀잼

---
+++ 2021-11-23/Lotto.js
@@ -0,0 +1,13 @@
+function lotto(drwNum,one,two,three,four,five,six,bns){
+
+    this.drwNum = drwNum
+    this.one = one
+    this.two = two
+    this.three = three
+    this.four = four
+    this.five = five
+    this.six = six
+    this.bns = bns
+}
+
+let lottos = []
